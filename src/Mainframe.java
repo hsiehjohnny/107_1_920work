0dd1c54373e9c9c026b0f80361b4d6ed3a655f43
hsiehjohnny <32057008+hsiehjohnny@users.noreply.github.com>
2018-09-27 09:25:26 +0800

    456

--- src/Mainframe.java
+++ src/Mainframe.java
@@ -3,10 +3,10 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class Mainframe extends JFrame {
-    private JTextField jtf = new JTextField();
+    private JPasswordField jtf = new JPasswordField();
     private String x = "23323456";
     private String str = new String("");
-    private JLabel jlb = new JLabel("");
+    //private JLabel jlb = new JLabel("");
     private Container cp;
     private JPanel jpl = new JPanel(new GridLayout(3, 4, 5, 5));
     private JButton jbt0 = new JButton("0");
@@ -29,30 +29,46 @@ public class Mainframe extends JFrame {
     public void init() {
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setBounds(100, 100, 800, 600);
+        this.setTitle("密碼輸入920work");
         cp = this.getContentPane();
         cp.setLayout(new BorderLayout(4, 4));
         cp.add(jtf, BorderLayout.NORTH);
         cp.add(jpl, BorderLayout.CENTER);
-        cp.add(jlb, BorderLayout.SOUTH);
+        // cp.add(jlb, BorderLayout.SOUTH);
+        jtf.setOpaque(true);
+        jtf.setBackground(new Color(250, 100, 60));
+        jtf.setFont(new Font(null, Font.BOLD, 30));
         jpl.add(jbt0);
+        jbt0.setFont(new Font(null, Font.PLAIN, 35));
         jpl.add(jbt1);
+        jbt1.setFont(new Font(null, Font.PLAIN, 35));
         jpl.add(jbt2);
+        jbt2.setFont(new Font(null, Font.PLAIN, 35));
         jpl.add(jbt3);
+        jbt3.setFont(new Font(null, Font.PLAIN, 35));
         jpl.add(jbt4);
+        jbt4.setFont(new Font(null, Font.PLAIN, 35));
         jpl.add(jbt5);
+        jbt5.setFont(new Font(null, Font.PLAIN, 35));
         jpl.add(jbt6);
+        jbt6.setFont(new Font(null, Font.PLAIN, 35));
         jpl.add(jbt7);
+        jbt7.setFont(new Font(null, Font.PLAIN, 35));
         jpl.add(jbt8);
+        jbt8.setFont(new Font(null, Font.PLAIN, 35));
         jpl.add(jbt9);
+        jbt9.setFont(new Font(null, Font.PLAIN, 35));
         jpl.add(jbt10);
+        jbt10.setFont(new Font(null, Font.PLAIN, 24));
         jpl.add(jbt11);
+        jbt11.setFont(new Font(null, Font.PLAIN, 24));
 
         jbt0.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 str = str + "0";
                 jtf.setText(jtf.getText() + "*");
-                jlb.setText(str);
+                // jlb.setText(str);
             }
         });
         jbt1.addActionListener(new AbstractAction() {
@@ -60,7 +76,7 @@ public class Mainframe extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 str = str + "1";
                 jtf.setText(jtf.getText() + "*");
-                jlb.setText(str);
+                // jlb.setText(str);
             }
         });
         jbt2.addActionListener(new AbstractAction() {
@@ -68,7 +84,7 @@ public class Mainframe extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 str = str + "2";
                 jtf.setText(jtf.getText() + "*");
-                jlb.setText(str);
+                //  jlb.setText(str);
             }
         });
         jbt3.addActionListener(new AbstractAction() {
@@ -76,7 +92,7 @@ public class Mainframe extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 str = str + "3";
                 jtf.setText(jtf.getText() + "*");
-                jlb.setText(str);
+                //     jlb.setText(str);
             }
         });
         jbt4.addActionListener(new AbstractAction() {
@@ -84,7 +100,7 @@ public class Mainframe extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 str = str + "4";
                 jtf.setText(jtf.getText() + "*");
-                jlb.setText(str);
+                //    jlb.setText(str);
             }
         });
         jbt5.addActionListener(new AbstractAction() {
@@ -92,7 +108,7 @@ public class Mainframe extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 str = str + "5";
                 jtf.setText(jtf.getText() + "*");
-                jlb.setText(str);
+                //  jlb.setText(str);
             }
         });
         jbt6.addActionListener(new AbstractAction() {
@@ -100,7 +116,7 @@ public class Mainframe extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 str = str + "6";
                 jtf.setText(jtf.getText() + "*");
-                jlb.setText(str);
+                //   jlb.setText(str);
             }
         });
         jbt7.addActionListener(new AbstractAction() {
@@ -108,7 +124,7 @@ public class Mainframe extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 str = str + "7";
                 jtf.setText(jtf.getText() + "*");
-                jlb.setText(str);
+                //   jlb.setText(str);
             }
         });
         jbt8.addActionListener(new AbstractAction() {
@@ -116,7 +132,8 @@ public class Mainframe extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 str = str + "8";
                 jtf.setText(jtf.getText() + "*");
-                jlb.setText(str);
+
+                //    jlb.setText(str);
             }
         });
         jbt9.addActionListener(new AbstractAction() {
@@ -124,7 +141,7 @@ public class Mainframe extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 str = str + "9";
                 jtf.setText(jtf.getText() + "*");
-                jlb.setText(str);
+                //   jlb.setText(str);
             }
         });
         jbt10.addActionListener(new AbstractAction() {
